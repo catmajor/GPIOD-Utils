482b49c43ef9e61872d7004c1671f8cd55741315
Nikita Ostapenko <nikitul2005@gmail.com>
2025-02-25 22:52:35 -0500

    :sparkles: refactor code to use std::optional, and also add more functions and stuff

--- shift_register/shift_register.cpp
+++ shift_register/shift_register.cpp
@@ -4,44 +4,47 @@ namespace gpiodutils {
 	using namespace std::chrono;
 	using namespace std::this_thread;
 	const int ONE = 1;
-	shift_register::shift_register(const std::string& chip_name, line_offset_int params, int num_outputs) : shift_register(chip{chip_name}, params, num_outputs){
+	shift_register::shift_register(const std::string& chip_name, offsets params, int num_outputs) : shift_register(chip{chip_name}, params, num_outputs){
 	}
-	shift_register::shift_register(const chip& chip_obj, line_offset_int params, int num_outputs)
+	shift_register::shift_register(const chip& chip_obj, offsets params, int num_outputs)
 	: num_outputs {num_outputs}, delay {nanoseconds(0)} {
 		init_lines(chip_obj, params);
 		num_output_bitmask = ((1 << num_outputs) - 1); 
 	}
-	shift_register::shift_register(line_offset_line_4 params, int num_outputs)
-	: shift_register({params.data, params.mem_clk, params.r_clk}, num_outputs) {
-		mem_clr_used = true;
-		MCLR = params.mem_clr;
-		MCLR.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 	
-	}
-	shift_register::shift_register(line_offset_line_3 params, int num_outputs)
+	shift_register::shift_register(lines params, int num_outputs)
 	: DATA {params.data}, MCLK {params.mem_clk}, RCLK {params.r_clk}, num_outputs {num_outputs}, delay {nanoseconds(0)} {
+		if (params.mem_clr) {
+			mem_clr_used = true;
+			MCLR = params.mem_clr.value();
+		} else {
+			mem_clr_used = false;
+		}
+		if (params.disp) {
+			disp_used = true;
+			DISP = params.disp.value();
+		} else {
+			disp_used = false;
+		}
 		num_output_bitmask = ((1 << num_outputs) - 1);
-		mem_clr_used = false;
-		DATA.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
-		MCLK.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
-		RCLK.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
-
 	}
-	void shift_register::init_lines(const chip& chip_obj, line_offset_int params) {
+	void shift_register::init_lines(const chip& chip_obj, offsets params) {
 		DATA = chip_obj.get_line(params.data_offset);
 		MCLK = chip_obj.get_line(params.mem_clk_offset);
 		RCLK = chip_obj.get_line(params.r_clk_offset);
-		if (params.mem_clr_offset >= 0) {
-			MCLR = chip_obj.get_line(params.mem_clr_offset);
-			MCLR.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 	
+		if (params.mem_clr_offset) {
+			MCLR = chip_obj.get_line(params.mem_clr_offset.value());
 			mem_clr_used = true;
 		} else {
 			mem_clr_used = false;
 		}
-		DATA.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
-		MCLK.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
-		RCLK.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
+		if (params.disp_offset) {
+			DISP = chip_obj.get_line(params.disp_offset.value());
+			disp_used = true;
+		} else {
+			disp_used = false;
+		}
 	}
-	void shift_register::set_val(int n) {
+	shift_register& shift_register::set_val(int n) {
 		n = num_output_bitmask & n;
 		cur_val = n;
 		if (mem_clr_used) {
@@ -64,15 +67,35 @@ namespace gpiodutils {
 		RCLK.set_value(1);
 		sleep_for(delay);
 		RCLK.set_value(0);
+		return *this;
+	}
+	shift_register& shift_register::enable_display(bool disp) {
+		if (disp_used) {
+			if (disp) {
+				DISP.set_value(0);
+			} else {
+				DISP.set_value(1);
+			}
+		}
+		return *this;
 	}
 	int shift_register::get_val() {
 		return cur_val;
 	}
-	template <typename Rep, typename Period>
-	void shift_register::set_delay(std::chrono::duration<Rep, Period> d) {
-		delay = duration_cast<nanoseconds>(d);
+	void shift_register::request() {
+		released = false;
+		DATA.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
+		MCLK.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
+		RCLK.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 
+		if (mem_clr_used) {
+			MCLR.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 	
+		}
+		if (disp_used) {
+			DISP.request({"shift register", line_request::DIRECTION_OUTPUT, 0},0); 	
+		}
 	}
-	shift_register::~shift_register() {
+	void shift_register::release() {
+		released = true;
 		DATA.set_value(0);
 		MCLK.set_value(0);
 		RCLK.set_value(0);
@@ -82,6 +105,16 @@ namespace gpiodutils {
 		if (mem_clr_used) {
 			MCLR.set_value(0);
 			MCLR.release();
+		} 
+		if (disp_used) {
+			DISP.set_value(0);
+			DISP.release();
+		}
+	
+	}
+	shift_register::~shift_register() {
+		if (!released) {
+			release();
 		}
 	}
 	shift_register& operator<<(shift_register& sr, int n) {
--- shift_register/shift_register.hpp
+++ shift_register/shift_register.hpp
@@ -4,51 +4,60 @@
 #include <chrono>
 #include <thread>
 #include <string>
+#include <optional>
 namespace gpiodutils {
 	using namespace gpiod;
 	using namespace std::chrono;
 	using namespace std::this_thread;
-	struct line_offset_int {
-		const int& data_offset;
-		const int& mem_clk_offset;
-	      	const int& r_clk_offset;
-		int mem_clr_offset = -1;	
-	};
-	struct line_offset_line_4 {
-		const line& data;
-		const line& mem_clk;
-		const line& r_clk;
-		const line& mem_clr;
-	};
-	struct line_offset_line_3 {
-		const line& data;
-		const line& mem_clk;
-		const line& r_clk;
-	};
+	using std::optional;
 	class shift_register {
 		public:
-			shift_register(const std::string&, line_offset_int, int num_outputs = 8);
-			shift_register(const chip&, line_offset_int, int num_outputs = 8);
-			shift_register(line_offset_line_4, int num_outputs = 8);
-			shift_register(line_offset_line_3, int num_outputs = 8);
-			void set_val(int);
+			struct offsets {
+				const int& data_offset;
+				const int& mem_clk_offset;
+				const int& r_clk_offset;
+				optional<int> mem_clr_offset;
+				optional<int> disp_offset;	
+			};
+			struct lines {
+				const line& data;
+				const line& mem_clk;
+				const line& r_clk;
+				optional<line> mem_clr;
+				optional<line> disp;
+			};
+			shift_register(const std::string&, offsets, int num_outputs = 8);
+			shift_register(const chip&, offsets, int num_outputs = 8);
+			shift_register(lines, int num_outputs = 8);
+			shift_register& set_val(int);
 			int get_val();
+			shift_register& enable_display(bool);
 			~shift_register();
+			void request();
+			void release();
 			template <typename Rep, typename Period>
-			void set_delay(std::chrono::duration<Rep, Period>);
+			void set_delay(std::chrono::duration<Rep, Period> d);
 		private:
-			void init_lines(const chip& chip, line_offset_int);
+			void init_lines(const chip& chip, offsets);
 			line DATA;
 			line MCLK;
 			line RCLK;
 			line MCLR;
+			line DISP;
 			int cur_val = 0;
 			int num_outputs = 8;
 			int num_output_bitmask = 0;
 			bool mem_clr_used = false;
+			bool disp_used = false;
+			bool released = true;
 			nanoseconds delay = nanoseconds(0);
 	};
 	shift_register& operator<<(shift_register&, int);
 	shift_register& operator>>(shift_register&, int&);
+	template <typename Rep, typename Period>
+	void shift_register::set_delay(std::chrono::duration<Rep, Period> d) {
+		delay = duration_cast<nanoseconds>(d);
+	}
 }
+
 #endif
